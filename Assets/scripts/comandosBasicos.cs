26dd12f2ad328e3dddfe59ccb00d3ec4f3f5f4d7
Unknown <luiz.otavio@ufvjm.edu.br>
2018-04-06 15:38:55 -0300

    Populando o BD com as palavras do '' Sol de Verão''

--- Assets/scripts/comandosBasicos.cs
+++ Assets/scripts/comandosBasicos.cs
@@ -466,6 +466,332 @@ public class bancoPalavras
 		palavras[0][31].nome_audio_ditado = "aviseditado";
 
 
+
+		/*
+
+		//NÍVEL 2 - S e Z!
+
+		// Palavra: Natal
+		palavras[0][32].palavra = "NATA_";
+		palavras[0][32].letra_correta = "L";
+		palavras[0][32].opcao1 = "U";
+		palavras[0][32].palavra_completa = "NATAL"; //Finalmente as férias chegaram! E junto com ela o Natal! Que legal!. 
+		palavras[0][32].nivel = 3;
+		palavras[0][32].nome_audio_menino = "natalmenino"; 
+		palavras[0][32].nome_audio_menina = "natalmenina";
+		palavras[0][32].nome_audio_ditado = "natalditado";
+
+		// Palavra: céu
+		palavras[0][33].palavra = "CÉ_";
+		palavras[0][33].letra_correta = "U";
+		palavras[0][33].opcao1 = "L";
+		palavras[0][33].palavra_completa = "CÉU"; //O céu fica azul no verão e o sol brilha. 
+		palavras[0][33].nivel = 3;
+		palavras[0][33].nome_audio_menino = "céumenino"; 
+		palavras[0][33].nome_audio_menina = "céumenina";
+		palavras[0][33].nome_audio_ditado = "céuditado";
+
+		// Palavra: especial
+		palavras[0][34].palavra = "ESPECIA_";
+		palavras[0][34].letra_correta = "L";
+		palavras[0][34].opcao1 = "U";
+		palavras[0][34].palavra_completa = "ESPECIAL"; //Tudo isso é muito especial!. 
+		palavras[0][34].nivel = 3;
+		palavras[0][34].nome_audio_menino = "especialmenino"; 
+		palavras[0][34].nome_audio_menina = "especialmenina";
+		palavras[0][34].nome_audio_ditado = "especialditado";
+
+		// Palavra: varal
+		palavras[0][35].palavra = "VARA_";
+		palavras[0][35].letra_correta = "L";
+		palavras[0][35].opcao1 = "U";
+		palavras[0][35].palavra_completa = "VARAL"; //Os irmãos fotografam a praia todos os dias, revelam as fotos e as colocam em um varal. 
+		palavras[0][35].nivel = 3;
+		palavras[0][35].nome_audio_menino = "varalmenino"; 
+		palavras[0][35].nome_audio_menina = "varalmenina";
+		palavras[0][35].nome_audio_ditado = "varalditado";
+
+		// Palavra: brasil
+		palavras[0][36].palavra = "BRASI_";
+		palavras[0][36].letra_correta = "L";
+		palavras[0][36].opcao1 = "U";
+		palavras[0][36].palavra_completa = "BRASIL"; //Marcos e Julinha moram no Brasil. 
+		palavras[0][36].nivel = 3;
+		palavras[0][36].nome_audio_menino = "brasilmenino"; 
+		palavras[0][36].nome_audio_menina = "brasilmenina";
+		palavras[0][36].nome_audio_ditado = "brasilditado";
+
+		// Palavra: quintal
+		palavras[0][37].palavra = "QUINTA_";
+		palavras[0][37].letra_correta = "L";
+		palavras[0][37].opcao1 = "U";
+		palavras[0][37].palavra_completa = "QUINTAL"; //No verão, a praia vira um quintal sensacional para eles. 
+		palavras[0][37].nivel = 3;
+		palavras[0][37].nome_audio_menino = "quintalmenino"; 
+		palavras[0][37].nome_audio_menina = "quintalmenina";
+		palavras[0][37].nome_audio_ditado = "quintalditado";
+
+		// Palavra: gentil
+		palavras[0][38].palavra = "GENTI_";
+		palavras[0][38].letra_correta = "L";
+		palavras[0][38].opcao1 = "U";
+		palavras[0][38].palavra_completa = "GENTIL"; //O vendedor de picolé é sempre gentil e dócil. 
+		palavras[0][38].nivel = 3;
+		palavras[0][38].nome_audio_menino = "gentilmenino"; 
+		palavras[0][38].nome_audio_menina = "gentilmenina";
+		palavras[0][38].nome_audio_ditado = "gentilditado";
+
+		// Palavra: dócil
+		palavras[0][39].palavra = "DÓCI_";
+		palavras[0][39].letra_correta = "L";
+		palavras[0][39].opcao1 = "U";
+		palavras[0][39].palavra_completa = "DÓCIL"; //O vendedor de picolé é sempre gentil e dócil. 
+		palavras[0][39].nivel = 3;
+		palavras[0][39].nome_audio_menino = "dócilmenino"; 
+		palavras[0][39].nome_audio_menina = "dócilmenina";
+		palavras[0][39].nome_audio_ditado = "dócilditado";
+
+		// Palavra: farol
+		palavras[0][40].palavra = "FARO_";
+		palavras[0][40].letra_correta = "L";
+		palavras[0][40].opcao1 = "U";
+		palavras[0][40].palavra_completa = "FAROL"; //O farol brilha à noite no mar. 
+		palavras[0][40].nivel = 3;
+		palavras[0][40].nome_audio_menino = "farolmenino"; 
+		palavras[0][40].nome_audio_menina = "farolmenina";
+		palavras[0][40].nome_audio_ditado = "farolditado";
+
+		// Palavra: fácil
+		palavras[0][41].palavra = "FÁCI_";
+		palavras[0][41].letra_correta = "L";
+		palavras[0][41].opcao1 = "U";
+		palavras[0][41].palavra_completa = "FÁCIL"; //Mas a diversão principal é muito fácil. 
+		palavras[0][41].nivel = 3;
+		palavras[0][41].nome_audio_menino = "fácilmenino"; 
+		palavras[0][41].nome_audio_menina = "fácilmenina";
+		palavras[0][41].nome_audio_ditado = "fácilditado";
+
+		// Palavra: túnel
+		palavras[0][42].palavra = "TÚNE_";
+		palavras[0][42].letra_correta = "L";
+		palavras[0][42].opcao1 = "U";
+		palavras[0][42].palavra_completa = "TÚNEL"; //Fazer túnel e castelo de areia. 
+		palavras[0][42].nivel = 3;
+		palavras[0][42].nome_audio_menino = "túnelmenino"; 
+		palavras[0][42].nome_audio_menina = "túnelmenina";
+		palavras[0][42].nome_audio_ditado = "túnelditado";
+
+		// Palavra: sinal
+		palavras[0][43].palavra = "SINA_";
+		palavras[0][43].letra_correta = "L";
+		palavras[0][43].opcao1 = "U";
+		palavras[0][43].palavra_completa = "SINAL"; //Hoje não havia nuvens no céu, e céu sem nuvens é sinal de muito sol. 
+		palavras[0][43].nivel = 3;
+		palavras[0][43].nome_audio_menino = "sinalmenino"; 
+		palavras[0][43].nome_audio_menina = "sinalmenina";
+		palavras[0][43].nome_audio_ditado = "sinalditado";
+
+		// Palavra: chapéu
+		palavras[0][44].palavra = "CHAPÉ_";
+		palavras[0][44].letra_correta = "U";
+		palavras[0][44].opcao1 = "L";
+		palavras[0][44].palavra_completa = "CHAPÉU"; //Usar chapéu e roupas leves é uma recomendação. 
+		palavras[0][44].nivel = 3;
+		palavras[0][44].nome_audio_menino = "chapéumenino"; 
+		palavras[0][44].nome_audio_menina = "chapéumenina";
+		palavras[0][44].nome_audio_ditado = "chapéuditado";
+
+		// Palavra: roupas
+		palavras[0][45].palavra = "RO_PAS";
+		palavras[0][45].letra_correta = "U";
+		palavras[0][45].opcao1 = "L";
+		palavras[0][45].palavra_completa = "ROUPAS"; //Usar chapéu e roupas leves é uma recomendação. 
+		palavras[0][45].nivel = 3;
+		palavras[0][45].nome_audio_menino = "roupasmenino"; 
+		palavras[0][45].nome_audio_menina = "roupasmenina";
+		palavras[0][45].nome_audio_ditado = "roupasditado";
+
+		// Palavra: seu
+		palavras[0][46].palavra = "SE_";
+		palavras[0][46].letra_correta = "U";
+		palavras[0][46].opcao1 = "L";
+		palavras[0][46].palavra_completa = "SEU"; //Ela tomou tanto sol, que seu corpo ficou febril. 
+		palavras[0][46].nivel = 3;
+		palavras[0][46].nome_audio_menino = "seumenino"; 
+		palavras[0][46].nome_audio_menina = "seumenina";
+		palavras[0][46].nome_audio_ditado = "seuditado";
+
+		// Palavra: animal
+		palavras[0][47].palavra = "ANIMA_";
+		palavras[0][47].letra_correta = "L";
+		palavras[0][47].opcao1 = "U";
+		palavras[0][47].palavra_completa = "ANIMAL"; //É preciso oferecer água até pro seu animalzinho 
+		palavras[0][47].nivel = 3;
+		palavras[0][47].nome_audio_menino = "animalmenino"; 
+		palavras[0][47].nome_audio_menina = "animalmenina";
+		palavras[0][47].nome_audio_ditado = "animalditado";
+
+		// Palavra: sal
+		palavras[0][48].palavra = "SA_";
+		palavras[0][48].letra_correta = "L";
+		palavras[0][48].opcao1 = "U";
+		palavras[0][48].palavra_completa = "SAL"; //Não pode ser água do mar porque contém muito sal. 
+		palavras[0][48].nivel = 3;
+		palavras[0][48].nome_audio_menino = "salmenino"; 
+		palavras[0][48].nome_audio_menina = "salmenina";
+		palavras[0][48].nome_audio_ditado = "salditado";
+
+		// Palavra: calça
+		palavras[0][49].palavra = "CA_ÇA";
+		palavras[0][49].letra_correta = "L";
+		palavras[0][49].opcao1 = "U";
+		palavras[0][49].palavra_completa = "CALÇA"; //Nesse dia, Marcos nadou de calça comprida. 
+		palavras[0][49].nivel = 3;
+		palavras[0][49].nome_audio_menino = "calçamenino"; 
+		palavras[0][49].nome_audio_menina = "calçamenina";
+		palavras[0][49].nome_audio_ditado = "calçaditado";
+
+		// Palavra: difícil
+		palavras[0][50].palavra = "DIFÍCI_";
+		palavras[0][50].letra_correta = "L";
+		palavras[0][50].opcao1 = "U";
+		palavras[0][50].palavra_completa = "DIFÍCIL"; //Foi muito difícil pular as ondas altas. 
+		palavras[0][50].nivel = 3;
+		palavras[0][50].nome_audio_menino = "difícilmenino"; 
+		palavras[0][50].nome_audio_menina = "difícilmenina";
+		palavras[0][50].nome_audio_ditado = "difícilditado";
+
+		// Palavra: altas
+		palavras[0][51].palavra = "A_TAS";
+		palavras[0][51].letra_correta = "L";
+		palavras[0][51].opcao1 = "U";
+		palavras[0][51].palavra_completa = "ALTAS"; //Foi muito difícil pular as ondas altas. 
+		palavras[0][51].nivel = 3;
+		palavras[0][51].nome_audio_menino = "altasmenino"; 
+		palavras[0][51].nome_audio_menina = "altasmenina";
+		palavras[0][51].nome_audio_ditado = "altasditado";
+
+		// Palavra: anzol
+		palavras[0][52].palavra = "ANZO_";
+		palavras[0][52].letra_correta = "L";
+		palavras[0][52].opcao1 = "U";
+		palavras[0][52].palavra_completa = "DIFÍCIL"; //Ele foi pescar e levou: anzol, iscas e um barquinho que não era de papel. 
+		palavras[0][52].nivel = 3;
+		palavras[0][52].nome_audio_menino = "anzolmenino"; 
+		palavras[0][52].nome_audio_menina = "anzolmenina";
+		palavras[0][52].nome_audio_ditado = "anzolditado";
+
+		// Palavra: papel
+		palavras[0][53].palavra = "PAPE_";
+		palavras[0][53].letra_correta = "L";
+		palavras[0][53].opcao1 = "U";
+		palavras[0][53].palavra_completa = "DIFÍCIL"; //Ele foi pescar e levou: anzol, iscas e um barquinho que não era de papel. 
+		palavras[0][53].nivel = 3;
+		palavras[0][53].nome_audio_menino = "papelmenino"; 
+		palavras[0][53].nome_audio_menina = "papelmenina";
+		palavras[0][53].nome_audio_ditado = "papelditado";
+
+		// Palavra: pneu
+		palavras[0][54].palavra = "PNE_";
+		palavras[0][54].letra_correta = "U";
+		palavras[0][54].opcao1 = "L";
+		palavras[0][54].palavra_completa = "PNEU"; //Marcos imaginou que o pneu era seu grande barco que quase virou nas ondas e bateu em um grande coral. 
+		palavras[0][54].nivel = 3;
+		palavras[0][54].nome_audio_menino = "pneumenino"; 
+		palavras[0][54].nome_audio_menina = "pneumenina";
+		palavras[0][54].nome_audio_ditado = "pneuditado";
+
+		// Palavra: coral
+		palavras[0][55].palavra = "CORA_";
+		palavras[0][55].letra_correta = "L";
+		palavras[0][55].opcao1 = "U";
+		palavras[0][55].palavra_completa = "CORAL"; //Marcos imaginou que o pneu era seu grande barco que quase virou nas ondas e bateu em um grande coral. 
+		palavras[0][55].nivel = 3;
+		palavras[0][55].nome_audio_menino = "coralmenino"; 
+		palavras[0][55].nome_audio_menina = "coralmenina";
+		palavras[0][55].nome_audio_ditado = "coralditado";
+
+		// Palavra: pau
+		palavras[0][56].palavra = "PA_";
+		palavras[0][56].letra_correta = "U";
+		palavras[0][56].opcao1 = "L";
+		palavras[0][56].palavra_completa = "PAU"; //Estava brincando de "pirata de perna de pau, de olho de vidro e da cara de mau". 
+		palavras[0][56].nivel = 3;
+		palavras[0][56].nome_audio_menino = "paumenino"; 
+		palavras[0][56].nome_audio_menina = "paumenina";
+		palavras[0][56].nome_audio_ditado = "pauditado";
+
+		// Palavra: mau
+		palavras[0][57].palavra = "MA_";
+		palavras[0][57].letra_correta = "U";
+		palavras[0][57].opcao1 = "L";
+		palavras[0][57].palavra_completa = "MAU"; //Marcos imaginou que o pneu era seu grande barco que quase virou nas ondas e bateu em um grande coral. 
+		palavras[0][57].nivel = 3;
+		palavras[0][57].nome_audio_menino = "maumenino"; 
+		palavras[0][57].nome_audio_menina = "maumenina";
+		palavras[0][57].nome_audio_ditado = "mauditado";
+
+		// Palavra: almanaque
+		palavras[0][58].palavra = "A_MANAQUE";
+		palavras[0][58].letra_correta = "L";
+		palavras[0][58].opcao1 = "U";
+		palavras[0][58].palavra_completa = "MAU"; //Marcos e Julinha possuem um almanaque que explica tudo sobre o fundo do mar. 
+		palavras[0][58].nivel = 3;
+		palavras[0][58].nome_audio_menino = "almanaquemenino"; 
+		palavras[0][58].nome_audio_menina = "almanaquemenina";
+		palavras[0][58].nome_audio_ditado = "almanaqueditado";
+
+		// Palavra: degraus
+		palavras[0][59].palavra = "DEGRA_S";
+		palavras[0][59].letra_correta = "U";
+		palavras[0][59].opcao1 = "L";
+		palavras[0][59].palavra_completa = "DEGRAUS"; //Quando anoitecer, os irmãos costumam subir os degraus para o andar superior. 
+		palavras[0][59].nivel = 3;
+		palavras[0][59].nome_audio_menino = "degrausmenino"; 
+		palavras[0][59].nome_audio_menina = "degrausmenina";
+		palavras[0][59].nome_audio_ditado = "degrausditado";
+
+		// Palavra: pouco
+		palavras[0][60].palavra = "PO_CO";
+		palavras[0][60].letra_correta = "U";
+		palavras[0][60].opcao1 = "L";
+		palavras[0][60].palavra_completa = "POUCO"; //pena que tudo que é bom, dura pouco... 
+		palavras[0][60].nivel = 3;
+		palavras[0][60].nome_audio_menino = "poucomenino"; 
+		palavras[0][60].nome_audio_menina = "poucomenina";
+		palavras[0][60].nome_audio_ditado = "poucoditado";
+
+		// Palavra: mingau
+		palavras[0][61].palavra = "MINGA_";
+		palavras[0][61].letra_correta = "U";
+		palavras[0][61].opcao1 = "L";
+		palavras[0][61].palavra_completa = "MINGAU"; //Agora está na hora de dormir, tomar mingau adoçadinho com mel 
+		palavras[0][61].nivel = 3;
+		palavras[0][61].nome_audio_menino = "mingaumenino"; 
+		palavras[0][61].nome_audio_menina = "mingaumenina";
+		palavras[0][61].nome_audio_ditado = "mingauditado";
+
+		// Palavra: mel
+		palavras[0][62].palavra = "ME_";
+		palavras[0][62].letra_correta = "L";
+		palavras[0][62].opcao1 = "U";
+		palavras[0][62].palavra_completa = "MEL"; //Agora está na hora de dormir, tomar mingau adoçadinho com mel 
+		palavras[0][62].nivel = 3;
+		palavras[0][62].nome_audio_menino = "melmenino"; 
+		palavras[0][62].nome_audio_menina = "melmenina";
+		palavras[0][62].nome_audio_ditado = "melditado";
+
+		// Palavra: final
+		palavras[0][63].palavra = "FINA_";
+		palavras[0][63].letra_correta = "L";
+		palavras[0][63].opcao1 = "U";
+		palavras[0][63].palavra_completa = "FINAL"; //acordar bem cedo para aproveitar o dia antes que as férias cheguem ao final
+		palavras[0][63].nivel = 3;
+		palavras[0][63].nome_audio_menino = "finalmenino"; 
+		palavras[0][63].nome_audio_menina = "finalmenina";
+		palavras[0][63].nome_audio_ditado = "finalditado";
+		*/
+
 		for (int i = 0; i < maxLevel; ++i)
           {
 
