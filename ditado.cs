b99d1f5e10bd90b46f20a438fff43bf3fb2a7b61
Grapphia <lupassis@gmail.com>
2018-06-07 21:37:15 -0300

    Add files via upload

---
+++ ditado.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+// Classe que controla todos os elementos gráficos do jogo!
+public class ditado : MonoBehaviour
+{
+
+	public GameObject animacaoProfessora;
+	//para animação de professora
+
+	private int idPalavra;
+
+	public InputField textoDigitado;
+
+	private int acertoPalavra, erroPalavra;
+
+	private int qtd_Palavras_Ditado = bancoPalavras.Instance.ListaIdPalavraAcerto.Count; 
+
+	private int posicao;
+
+	public Text quantidadePalavrasApresentadas;
+
+	public GameObject orientacaoInicial;
+
+	private bool mensagemInicial_Aplicada = false;
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	// Inicializa a tela do jogo!
+	void Start ()
+	{
+		//Desativar o aparecimento constante da mensagem de orientação
+		if (mensagemInicial_Aplicada == false) {
+			orientacaoInicial.SetActive (true);
+		} else {
+			orientacaoInicial.SetActive (false);
+		}
+
+		Debug.Log (qtd_Palavras_Ditado);
+		posicao = Random.Range (0, qtd_Palavras_Ditado);
+		idPalavra = bancoPalavras.Instance.ListaIdPalavraAcerto[posicao];
+		bancoPalavras.Instance.ListaIdPalavraAcerto.RemoveAt (posicao);
+		qtd_Palavras_Ditado--;
+	}
+
+
+	//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	//Função criada por Magno 
+	public void PegaTexto (){		
+		string palavraAnalisada = bancoPalavras.Instance.palavras[idPalavra].palavra_completa;
+		string texto = textoDigitado.text;
+		string upperString = texto.ToUpper();
+
+		if (palavraAnalisada == upperString) {
+			Debug.Log ("Acertou");
+			++dadosJogo.Instance.currentUser.scoreDitado;
+			textoDigitado.text = "";
+		} else {
+			++dadosJogo.Instance.currentUser.erroDitado;
+			erroPalavra++;
+			textoDigitado.text = "";
+		}
+
+		int quantidadePalavrasAtual = bancoPalavras.Instance.numWordsDitado - 1;
+		quantidadePalavrasApresentadas.text = quantidadePalavrasAtual.ToString();
+		quantidadePalavrasAtual--;
+
+		bancoPalavras.Instance.numWordsDitado--;
+		StartCoroutine ("Start");
+
+		//Definir o numero de palavras no ditado no comandosBasicos.cs
+		if (bancoPalavras.Instance.numWordsDitado == 0) {
+			//volta para 5 palavras, caso opte por jogar novamente no mesmo usuário já que essa variável é decrementada
+			//durante o processo do ditado
+			bancoPalavras.Instance.numWordsDitado = 5;
+			//toda vez que o jogo é executado a lista é novamente preenchida com todas as palavras do banco acrescida das palavras do jogo
+			bancoPalavras.Instance.ListaIdPalavraAcerto.Clear();
+			SceneManager.LoadScene ("telaFimJogo");
+		}
+	}
+
+	//Função criada por Magno
+	public void pressedAudioPalavra (){
+		orientacaoInicial.SetActive (false);
+		mensagemInicial_Aplicada = true;
+
+		animacaoProfessora.SetActive (true);
+		StartCoroutine ("audioEnd");
+		string arquivo = "audiosditado/" + bancoPalavras.Instance.palavras[idPalavra].nome_audio_ditado;
+
+		AudioClip clip = (AudioClip)Resources.Load (arquivo);
+		AudioSource audio;
+		audio = gameObject.AddComponent<AudioSource> ();
+		audio.volume = 1;
+		audio.clip = clip;
+		audio.Play ();
+	}	
+
+	IEnumerator audioEnd(){
+		string arquivo = "audiosditado/" + bancoPalavras.Instance.palavras[idPalavra].nome_audio_ditado;
+
+		AudioClip clip = (AudioClip)Resources.Load (arquivo);
+		yield return new WaitForSeconds (clip.length);
+		animacaoProfessora.SetActive (false);
+	}
+}
---
+++ enviaDados.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Mail;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class enviaDados : MonoBehaviour {
+	public GameObject somOn;
+	public GameObject somOff;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	//Função criada por Magno
+	public void SalvaDadosJogado(){
+		//Salvar txt para anexo
+		int idUsuario = dadosJogo.Instance.currentUser.Id;
+		string nomeUsuario = dadosJogo.Instance.currentUser.Name;
+		int pontuacaoUsuarioAcerto = dadosJogo.Instance.currentUser.Score;
+		int pontuacaoUsuarioAcertoDitado = dadosJogo.Instance.currentUser.scoreDitado;
+		int pontuacaoUsuarioErroDitado = dadosJogo.Instance.currentUser.erroDitado;
+
+		StreamWriter sw = new StreamWriter (Application.persistentDataPath + nomeUsuario + " - Pontuacao.txt");
+		sw.WriteLine ("Id do Usuário: " + idUsuario);
+		sw.WriteLine ("Nome do Usuário: " + nomeUsuario);
+		sw.WriteLine ("Acertos do Usuário: " + pontuacaoUsuarioAcerto);
+		sw.WriteLine ("Acertos do Usuário no Ditado: " + pontuacaoUsuarioAcertoDitado);
+		sw.WriteLine ("Erros do Usuário no Ditado: " + pontuacaoUsuarioErroDitado);
+		sw.WriteLine ("Palavras Acerto: ");
+		for(int i=0; i<bancoPalavras.Instance.palavrasAcerto.Length; ++i){
+			sw.WriteLine (bancoPalavras.Instance.palavrasAcerto[i].idPalavra + " - " + bancoPalavras.Instance.palavrasAcerto[i].acerto);
+		}
+		sw.Close ();
+	}
+
+	//Função criada por Magno
+	/*Função para envio automatico de e-mail ao clicar em salvar dados do jogo, o e-mail será enviado para projetograpphia@gmail.com*/
+	public void EnviarEmail ()
+	{
+		//Código para enviar email utilizando o protocolo smtp
+		MailMessage mail = new MailMessage ();
+		string nomeUsuario = dadosJogo.Instance.currentUser.Name;
+
+		StartCoroutine ("SalvaDadosJogado");
+
+		try {
+			mail.From = new MailAddress ("projetograpphia@gmail.com");
+			mail.To.Add ("projetograpphia@gmail.com");
+			mail.Subject = "Pontuação do usuário: " + nomeUsuario;
+			mail.Body = "Estes são os dados de jogo do(a): " + nomeUsuario ;
+			mail.Attachments.Add (new Attachment (Application.persistentDataPath + nomeUsuario + " - Pontuacao.txt"));//anexo
+
+			SmtpClient smtpServer = new SmtpClient ();
+			smtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+			smtpServer.Port = 587;
+			smtpServer.Host = "smtp.gmail.com";
+			smtpServer.Credentials = new System.Net.NetworkCredential ("projetograpphia@gmail.com", "grapphia2017") as ICredentialsByHost;
+			smtpServer.EnableSsl = true;
+			ServicePointManager.ServerCertificateValidationCallback = 
+				delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+				return true;
+			};
+			smtpServer.Send (mail);
+		} catch (SmtpException ex) {
+			Debug.Log ("Exception: " + ex);
+		}
+
+		SceneManager.LoadScene ("telaEstante");
+	}
+
+	public void click_Sound()
+	{
+		// Função que bloqueia e ativa o som!
+		var compaudio = somOn.GetComponent<AudioSource>();
+
+		if (compaudio.isPlaying)
+		{
+			somOn.SetActive(false);
+			somOn.GetComponent<AudioSource>().Pause();
+			somOff.SetActive(true);
+		}
+		else
+		{
+			somOn.SetActive(true);
+			somOn.GetComponent<AudioSource>().UnPause();
+			somOff.SetActive(false);
+		}
+
+	}
+}
